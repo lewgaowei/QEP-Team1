11073162c319ae48a16d7cb9002d21ef18afcc85
zwnkoko <zwnkoko@gmail.com>
2023-11-15 19:31:47 +0800

    remove commented codes

--- project.py
+++ project.py
@@ -10,19 +10,3 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
-
-#connection, cursor = connect_to_db()
-#print(f"Block size of database : {get_block_size(cursor)} bytes")
-
-#print(get_query_plan(cursor, "SELECT * from nation, customer;"))
-
-#print(get_buffer_hits(cursor, "SELECT * from nation, customer;"))
-
-
-#cursor.execute(execution_plan_query)
-
-#plan = cursor.fetchall()
-#plan = plan[0][0][0]['Plan']
-#print(plan)
-
